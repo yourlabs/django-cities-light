e83d7bfe503b15aba69595aaa2869404f2581d10
Arseny Sokolov <me@arsen.pw>
2020-03-17 21:06:02 +0800

    Fix Django 3.0 support.

--- cities_light/abstract_models.py
+++ cities_light/abstract_models.py
@@ -5,7 +5,10 @@ import re
 import autoslug
 import pytz
 
-from six import python_2_unicode_compatible
+try:
+    from six import python_2_unicode_compatible
+except ImportError:
+    from django.utils.encoding import python_2_unicode_compatible
 
 from django.db import models
 from django.db.models import lookups
